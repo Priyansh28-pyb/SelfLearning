0d98bc6e954c309d51652ba630e4d7815a834fd6
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 17:17:59 +0530

    first program hello world and some variations of printing

---
+++ PythonFundamentals1/01_Firstprogram/first_program.py
@@ -0,0 +1,14 @@
+#print hello world
+print("Hello World")
+
+#print multiple lines / strings
+print("first string")
+print("second string")
+print("Third string")
+
+#print 2 strings in single line using comma ,
+print("hello world" , "Priyanshu")
+
+#next line print the string using\n
+
+print("Hello \nWorld")
