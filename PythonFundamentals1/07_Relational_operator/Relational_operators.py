fcc6119489c8fb388881ba73f7048e9f4d45383f
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 18:50:05 +0530

    relational  operations are performed on a n b n c

---
+++ PythonFundamentals1/07_Relational_operator/Relational_operators.py
@@ -0,0 +1,18 @@
+#relational or comparison operators are used for comparing the two values
+#we will get true or false in return
+
+a = 20
+b = 30
+c = 20
+
+#greater than or greater than equal to
+print(a > b)
+print(a >= c)
+
+#less than or less than equal to
+print(a<b)
+print(a<=b)
+
+#equal to equal to or not equal to
+print(a==c)
+print(a!=b)
