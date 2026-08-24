c4d28d11f9e052466a4eb312c51ec8f725f51919
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 17:42:40 +0530

    creating variables and variations of printing variables in different form

---
+++ PythonFundamentals1/02_Variables_in_python/variables.py
@@ -0,0 +1,17 @@
+#variables are the containers that store the data or value
+
+
+#name , age are variable
+name = "Priyanshu Singh"
+age = 29
+
+print(name , age )
+
+#another way of printing the variables
+
+print("My name is : " , name , "My age is : " , age)
+print("My name is : " , name , "\nMy age is : " , age)
+
+#can perform operations on varibale like add subtract multiply
+
+print("My age after 5 years : ", age + 5)
