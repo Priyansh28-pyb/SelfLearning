dfd4a9e9ccdfd8161ac2d2260a858bc835e2fe08
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 19:04:09 +0530

    assignment operations are performed

---
+++ PythonFundamentals1/08_Assignment_operator/Assignment_operator.py
@@ -0,0 +1,18 @@
+#assignment operator is used for the assigning the value
+
+# 5 is assigning to the a
+a = 5
+
+a += 1
+#now a is 6
+print(a)
+
+a -= 1
+print(a)
+
+a *= 2
+print(a)
+
+a %= 2
+print(a)
+
