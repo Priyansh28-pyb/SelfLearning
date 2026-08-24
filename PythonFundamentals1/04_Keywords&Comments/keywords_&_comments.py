b907fe0d2e88fa2788936c7d1c8ecbfea6e4b8a2
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 18:12:36 +0530

    keywords and comments are described in this code with example

---
+++ PythonFundamentals1/04_Keywords&Comments/keywords_&_comments.py
@@ -0,0 +1,16 @@
+#keywords are the reserved words that are not use for the making of variable
+
+#True = "this will show error"
+
+#comments are made by using hash symbol its ignore by interpreter
+
+'''
+multi line comments are
+made by using triple qoutes in python
+
+comments are used for describing our code for better usnderstanding by
+any devloper or who is
+viewing the code
+'''
+
+print("Keywords and comments are described in above code")
