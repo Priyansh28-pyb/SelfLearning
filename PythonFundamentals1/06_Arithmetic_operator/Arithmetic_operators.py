09b9711ae608d705d04e03c5a9f067a0f10118b0
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 18:38:01 +0530

    arithmatic operations are performed on a n b

---
+++ PythonFundamentals1/06_Arithmetic_operator/Arithmetic_operators.py
@@ -0,0 +1,12 @@
+#arithmetic operators are used for perform the mathmatical operations on the variables
+#like sum substraction divide multiply moduls power
+
+a = 9
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #modulus
+print(a**b) #power
