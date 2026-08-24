cd3b95297d89fafeef1088c5dcd5c4fd1d6e9be0
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 19:41:01 +0530

    logical operations are performed

---
+++ PythonFundamentals1/09_Logical_operator.py
@@ -0,0 +1,12 @@
+#using not operator reverse the result
+print(not(1==1))
+print(not(1==2))
+
+#using and opertor
+print((1==1)) and print((1==1))
+print(not(1==1)) and print((1==1))
+# both condition must be true in this and logical opertor for returing the true
+
+#using or opertor
+print((1==1) or (1==1))
+print(not(1==1)) or print((1==1))
