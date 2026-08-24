f75d0f9d529873a89233ee608455d092037c7935
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 18:26:11 +0530

    different style case used and described in this code

---
+++ PythonFundamentals1/05_Style_guide/style_guide.py
@@ -0,0 +1,13 @@
+#style guide is used for naming the variable in python with different styles
+
+#here words in small and under score is used for next word
+my_name = "this snake_case (preffered)"
+
+# here name start with small letter and new word start with capital letter without spcae
+myName = "This is camelCase"
+
+#here every new word start with capital letter without spcae
+
+MyName = "This is Pascalcase"
+
+print( my_name , myName , MyName)
