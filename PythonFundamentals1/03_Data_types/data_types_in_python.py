4b3ea90291cf74c2dd4a315a8047a091df191af7
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-24 18:01:26 +0530

    data types and its types and printing of data types

---
+++ PythonFundamentals1/03_Data_types/data_types_in_python.py
@@ -0,0 +1,19 @@
+#data types will tell about the type of data stored in a variable
+
+name = "Priyanshu  Singh"
+age = 21
+weight = 75.2
+
+#type function will be used for geting the data type of the variable along with print
+
+print(type(name))
+print(type(age))
+print(type(weight))
+
+#boolean data type used only store True False with case sensitive
+
+num = 2
+isPrime = True
+print(type(isPrime))
+
+#jis variable ko kuch assign nahi krte usko None kr skte h
