2a2bdb7901a6caeac225c0761922b6962471bbf0
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 21:13:46 +0530

    taking input from users and convert the inputs for perform sum function

---
+++ PythonFundamentals1/12_Taking_user_input/Taking_user_input.py
@@ -0,0 +1,20 @@
+#Taking input from users by using input function()
+
+name = input("Enter your name :")
+print("Welcome " , name)
+
+#performing sum opertion on inputs
+
+num1 = input("Enter a number 1 :")
+num2 = input("Enter a number 2 :")
+print(num1 + num2)
+
+#it will concatinate the input becasue input function take default in string type
+#so we have to convert the input using int function or according to need
+
+a = int(input("Enter a number :"))
+b = int(input ("Enter 2nd number :"))
+print(a + b)
+
+#agr float mae input askta h to float function use kro nahi toh error ayega
+#jb user int wale mae float input de dega toh
