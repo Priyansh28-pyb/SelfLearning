31e159ed096a6d10b0f2f12361f48246bc2a411a
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 21:49:20 +0530

    task 3 complete

---
+++ PythonFundamentals1/14_Assignment_problems/03_Task.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter a integer :"))
+num2 = float(input("Enter another integer : "))
+num3 = float(input("Enter a float :"))
+
+avg = (num1 +num2 + num3)/ 3
+print("num1 is : ", num1 , "num2 is : " , num2 , "num3 is :" ,num3)
+print("Average of 3 numbers is : " , avg)
