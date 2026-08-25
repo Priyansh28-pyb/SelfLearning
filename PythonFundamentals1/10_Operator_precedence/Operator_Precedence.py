c2b90963203888bf1aa183bc35ca138ab0e918aa
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 20:39:19 +0530

    operator precesion tested

---
+++ PythonFundamentals1/10_Operator_precedence/Operator_Precedence.py
@@ -0,0 +1,7 @@
+#operator precedence mae priorty of operator batata hae , which operator will perform operation first
+#similar to Bodomas
+#if same priority opertor occurs together will perform operation from left to right
+
+print(3*3/7+1-4**3)
+
+#logical opertor will in last not and or in same order
