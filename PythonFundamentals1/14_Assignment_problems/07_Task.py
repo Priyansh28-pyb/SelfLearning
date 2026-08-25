19411f74e26b3f22051ef6b30b0c1a081008eb00
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 22:17:36 +0530

    task 7 complete : taking input of temperature and convert to fahrenheit

---
+++ PythonFundamentals1/14_Assignment_problems/07_Task.py
@@ -0,0 +1,11 @@
+#taking temperature input from user and convert to float and feranite
+
+temp = input("Enter temperature in Celsius :")
+
+a = float(temp)
+
+print("Temperature in float : " ,a)
+
+b = (a * (9/5))+ 32
+
+print("Temperature in Faherenheit :",b)
