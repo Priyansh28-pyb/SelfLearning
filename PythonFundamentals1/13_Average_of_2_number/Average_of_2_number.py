d1252bb414f768c257070071be5edcbd8693fc14
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 21:21:49 +0530

    program of avg of two numbers of float by taking user input

---
+++ PythonFundamentals1/13_Average_of_2_number/Average_of_2_number.py
@@ -0,0 +1,8 @@
+#average of 2 numbers using float values
+
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter Second number : "))
+
+avg = ((num1 + num2)/2)
+
+print("Average of two number is : " , avg)
