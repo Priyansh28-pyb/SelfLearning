02f7a739dfa9c8fadfab3aff8312a052ce77adf0
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 21:58:03 +0530

    task 4 complete

---
+++ PythonFundamentals1/14_Assignment_problems/04_Task.py
@@ -0,0 +1,5 @@
+num =  input("Enter a number (as string input is taken) :")
+
+print("Convert it to an integrer :",int(num))
+print("Converted to float :",float(num))
+print("Converted to String again :",str(num))
