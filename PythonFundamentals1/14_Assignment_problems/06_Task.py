3555fbf5809f3ce69d39ffc625cecf33e5898e62
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-25 22:09:31 +0530

    task 6 complete : sawpping of number entered by user

---
+++ PythonFundamentals1/14_Assignment_problems/06_Task.py
@@ -0,0 +1,16 @@
+#swaping two numbers entered by the user
+
+num1 = int(input("Enter num 1 :"))
+num2 = int(input("Enter num 2 :"))
+
+
+print("number 1 is : ", num1)
+print("number 2 is : ", num2)
+
+print("Numbers after swapping")
+num3 = num1
+num1 = num2
+num2 = num3
+
+print("number 1 is :" ,num1)
+print("number 2 is :" ,num2)
