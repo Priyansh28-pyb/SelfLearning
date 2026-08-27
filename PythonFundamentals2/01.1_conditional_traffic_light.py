83d8e52f1b3b9f652c567442b5d32cd15cda269c
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-27 21:43:41 +0530

    conditional statement code for traffic light

---
+++ PythonFundamentals2/01.1_conditional_traffic_light.py
@@ -0,0 +1,13 @@
+color = input("Enter a color:")
+
+if color == "red":
+    print("STOP")
+
+elif color == "green":
+    print("GO")
+
+elif color == "yellow":
+    print("WAIT")
+
+else :
+    print("Enter a valid color for traffic light")
