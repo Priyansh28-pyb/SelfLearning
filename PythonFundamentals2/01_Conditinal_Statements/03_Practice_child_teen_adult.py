49f6fdb7dc96b3001e0b1baf5f0490023e07b39a
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-27 21:56:00 +0530

    Practice example done for teenage adult and child

--- PythonFundamentals2/01_Conditinal_Statements/03_Practice_child_teen_adult.py
+++ PythonFundamentals2/01_Conditinal_Statements/03_Practice_child_teen_adult.py
@@ -3,7 +3,7 @@ age = int(input("Enter your age : "))
 if age < 13 and age > 0:
     print("You are child")
 
-elif age > 13 and age < 18 :
+elif age >= 13 and age < 18 :
     print("You are teenager")
 
 elif age >= 18 and age < 100 :
