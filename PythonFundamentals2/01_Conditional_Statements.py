59598a83addde55405eec742b3f3ec55afe0a21e
Priyansh28-pyb <livepriyansh@gmail.com>
2026-08-27 21:39:06 +0530

    conditional statement code for checking AGE FOR VOTING AND DRIVING

---
+++ PythonFundamentals2/01_Conditional_Statements.py
@@ -0,0 +1,10 @@
+#conditional statements uses if else elif for checking and executing some program after condition check
+
+age =  int(input("Enter your age :"))
+
+if age >= 18:
+    print("You can vote")
+    print("You can drive")
+
+else:
+    print("You can't vote and drive")
