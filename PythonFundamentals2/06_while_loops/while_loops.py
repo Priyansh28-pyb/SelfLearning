077484708082ede41f277b04d9aa704f375776c9
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-05 11:50:18 +0530

    program for printing 5 time hello world

---
+++ PythonFundamentals2/06_while_loops/while_loops.py
@@ -0,0 +1,9 @@
+#koi same kaam agr baar baar krna ho to loops use krte h
+#while loops phle condtion check krte h fir kaam krte h jb tk condition true hoti h
+
+#print 5 times hello world
+
+i = 1
+while i <= 5 :
+    print("Hello world")
+    i += 1
