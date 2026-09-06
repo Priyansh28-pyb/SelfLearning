0a478f51ee86f03122bad3889c3e19f1af8b3064
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 21:20:25 +0530

    factorial of n numbers

---
+++ PythonFundamentals2/17_factorial_of_n/factorial_function.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number : "))
+
+def factorial(n):
+    if n == 0:
+        return 1
+    fact = 1
+    for i in range(1,n+1):
+        fact *= i
+
+    return fact
+
+print(factorial(n))
