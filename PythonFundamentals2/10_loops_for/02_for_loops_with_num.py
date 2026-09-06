2ccf076f560a6cc2e790644501af71ac7891adc5
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 20:01:24 +0530

    for loops with range and if with "in"

---
+++ PythonFundamentals2/10_loops_for/02_for_loops_with_num.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+    print(i+1)
+
+#range k andr jo likha h wha tk num print hogyega 0 sae leke n-1 tk
+#in operator also will be used for checking the any character is present or not can be used with conditional statements
+
+words = "hellooo"
+
+if "o" in words:
+    print("o exists")
