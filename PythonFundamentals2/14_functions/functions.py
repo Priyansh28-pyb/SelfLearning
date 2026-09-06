305a187f18add455e3b7585660564a6915671b61
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 20:53:28 +0530

    functions amd some examples

---
+++ PythonFundamentals2/14_functions/functions.py
@@ -0,0 +1,15 @@
+#functions are block of statements that are perform some specific task
+
+def hello():
+    print("Hello , this is python program ")
+    #function defination
+
+#hello() # function call
+
+
+# parameters
+def add(a,b):
+    s = a + b
+    return s
+
+add(6,7)#arguments
