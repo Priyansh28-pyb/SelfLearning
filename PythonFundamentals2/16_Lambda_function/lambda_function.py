3684030a7282b1854fe9bc184a3c3d904b94892c
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 21:13:52 +0530

    lambda functions practice

---
+++ PythonFundamentals2/16_Lambda_function/lambda_function.py
@@ -0,0 +1,9 @@
+#used for smalll functions or single line expersions
+
+sum = lambda a , b , c : a+b+c
+avg = lambda a , b: (a+b)/2
+#isme only single line expersion hota h
+
+
+print(sum(4,5,1))
+print(avg(2,3))
