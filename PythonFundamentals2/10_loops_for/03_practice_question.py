62a59589ec39ae07f7da6164056aa8b92ba72bfb
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 20:04:55 +0530

    i count occur check in ai spelling

---
+++ PythonFundamentals2/10_loops_for/03_practice_question.py
@@ -0,0 +1,11 @@
+#check how many time i will occur
+
+string = "artificial intellingence"
+
+ans =0
+
+for ch in string:
+    if(ch == "i"):
+        ans += 1
+
+print("i occurs at " , ans , " times")
