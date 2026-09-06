d15635dd0c32f52a57e510e4c138abc0deaf6183
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 19:55:55 +0530

    for loops for the string

---
+++ PythonFundamentals2/10_loops_for/01_for_loops.py
@@ -0,0 +1,9 @@
+#for loops are used for the seqeuential traversal in string or in num seq
+
+string = "hello"
+
+for var in string :
+    print(var)
+
+
+#in operator is used for checking the presence in seqeunce
