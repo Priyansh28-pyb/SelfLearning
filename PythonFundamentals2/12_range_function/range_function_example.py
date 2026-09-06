1ad7d572f6ad4b487e8877e7113bb55a5ac6ffbc
Priyansh28-pyb <livepriyansh@gmail.com>
2026-09-06 20:18:34 +0530

    range function exploration

---
+++ PythonFundamentals2/12_range_function/range_function_example.py
@@ -0,0 +1,14 @@
+#range function is used for genrating the sequences
+
+for i in range(5):
+    print(i)
+
+for j in range(1,6):
+    print(j)
+    #isme humne start value bhi add krdi h ab to ye 1 sae suru hoga
+
+for k in range(1,10,2):
+    print(k)
+    #isme humne step value bhi add krdi h 2
+
+#range mae stop value dena compulsary hota h or start ki default value 0 hoti or step 1
